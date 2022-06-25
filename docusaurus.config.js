7fbd508e0e59bd604e89e2e8cc5b71c67e60e7fe
merunno <merunno92@gmail.com>
2022-06-26 00:32:16 +0900

    chore: set config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,25 +6,25 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'SpicyAzisaTicket Docs',
+  tagline: 'SpicyAzisaTicket Document',
+  url: 'https://spicy-docs.m2en.dev',
   baseUrl: '/',
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  onBrokenMarkdownLinks: 'log',
+  favicon: 'https://github.com/azisaba.png',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'AzisabaNetwork & m2en', // Usually your GitHub org/user name.
+  projectName: 'spicy-azisa-ticket', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'ja',
+    locales: ['ja'],
   },
 
   presets: [
